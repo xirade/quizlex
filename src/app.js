1ebe799c120023a2cb80b18a490ec42c0bfce805
xirade <6myusic6@gmail.com>
2021-11-17 11:41:19 +0200

    add modal.scss component, refactor scripts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import "./styles.scss";
 import modalWindow from "./modules/module_window";
+import questionModule from "./modules/module_question";
+
+const cards = [
+  {
+    id: 1,
+    title: "Central Quality Architect",
+    description: "Senior",
+    img: "http://placeimg.com/640/480/abstract",
+  },
+  {
+    id: 2,
+    title: "Regional Creative Assistant",
+    description: "Dynamic",
+    img: "http://placeimg.com/640/480/animals",
+  },
+];
 
 const container = document.querySelector(".container");
+const row = container.querySelector(".row");
 const { buttonModal, divModalWindow } = modalWindow();
+const { questionCardsRender } = questionModule();
 
-container.append(buttonModal, divModalWindow);
+const questionCards = questionCardsRender(cards);
+row.append(...questionCards);
+container.append(row, buttonModal, divModalWindow);
